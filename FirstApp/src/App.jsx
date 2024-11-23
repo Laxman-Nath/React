a6480be85c89ef0c498f6a25eda42ed5b668469f
Laxman-Nath <nathlaxman242@gmail.com>
2024-11-23 20:54:26 +0545

    learned about using dynamic values in jsx

--- FirstApp/src/App.jsx
+++ FirstApp/src/App.jsx
@@ -43,32 +43,47 @@ const App = () => {
 export default App;
 
 export const NetFlixSeries = () => {
+  const name = "Queen of tears";
+  const rating = 8.9;
+  const src = "1.jpg";
+  const summary =
+    "Queen of Tears (Korean: 눈물의 여왕) is a 2024 " +
+    "South Korean television series written by Park Ji-eun, co-directed by " +
+    " Jang Young-woo [ko] and Kim Hee-won, and starring Kim Soo-hyun, Kim " +
+    "  Ji-won, Park Sung-hoon, Kwak Dong-yeon, and Lee Joo-bin. The series " +
+    "   depicts the story of a married couple in a crisis. It aired on tvN from " +
+    "   March 9, to April 28, 2024, every Saturday and Sunday at 21:20 (KST). It " +
+    " is also available for streaming on TVING in South Korea, and on Netflix " +
+    "   in selected regions. Queen of Tears reached a nationwide rating of " +
+    "  24.850% for its final episode and became the highest-rated tvN series, " +
+    "  surpassing Crash Landing on You.[4] It also became the third " +
+    "  highest-rated series in Korean cable television history for viewership " +
+    " ratings and the second highest by number of viewers.";
   return (
-    <div style={{ textAlign: "center", fontSize: "20px" ,marginBottom:"10px",marginTop:"5px"}} >
+    <div
+      style={{
+        textAlign: "center",
+        fontSize: "20px",
+        marginBottom: "10px",
+        marginTop: "5px",
+      }}
+    >
       <div style={{ textAlign: "center" }}>
-        <img src="1.jpg" alt="error" height={400} width={600} />
-        
+        <img src={src} alt="error" height={400} width={600} />
       </div>
-     
+
       <p>
-        <strong style={{color:"green",fontSize:"30px"}}>Name:</strong>Queen of tears
+        <strong style={{ color: "green", fontSize: "30px" }}>Name:</strong>
+        {name}
       </p>
       <p>
-        <strong style={{color:"green",fontSize:"30px"}}>Rating:</strong>8.9
+        <strong style={{ color: "green", fontSize: "30px" }}>Rating:</strong>
+        {/* {rating} */}
+        {8+0.2}
       </p>
       <p style={{ width: "600px", margin: "auto" }}>
-        <strong style={{color:"green",fontSize:"30px"}}>Summary:</strong> Queen of Tears (Korean: 눈물의 여왕) is a 2024
-        South Korean television series written by Park Ji-eun, co-directed by
-        Jang Young-woo [ko] and Kim Hee-won, and starring Kim Soo-hyun, Kim
-        Ji-won, Park Sung-hoon, Kwak Dong-yeon, and Lee Joo-bin. The series
-        depicts the story of a married couple in a crisis. It aired on tvN from
-        March 9, to April 28, 2024, every Saturday and Sunday at 21:20 (KST). It
-        is also available for streaming on TVING in South Korea, and on Netflix
-        in selected regions. Queen of Tears reached a nationwide rating of
-        24.850% for its final episode and became the highest-rated tvN series,
-        surpassing Crash Landing on You.[4] It also became the third
-        highest-rated series in Korean cable television history for viewership
-        ratings and the second highest by number of viewers.
+        <strong style={{ color: "green", fontSize: "30px" }}>Summary:</strong>
+        {summary}
       </p>
       <hr style={{ height: "5px", backgroundColor: "black" }} />
     </div>
