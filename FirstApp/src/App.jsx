15097930e20e5df250b08577907707ba99aa61d3
Laxman-Nath <nathlaxman242@gmail.com>
2024-11-23 16:30:38 +0545

    leared about components in react

--- FirstApp/src/App.jsx
+++ FirstApp/src/App.jsx
@@ -1,13 +1,38 @@
-// const App=()=>{
-//   return <h1>This is the react app created By laxman nath.</h1>
-// }
-
-import React from "react";
-
-const App=()=>{
-  console.log(React.createElement("h1",null,"Hello my name is laxman nath"));
-  return  React.createElement("h1",null,"Hello my name is laxman nath");
-
-}
+const App = () => {
+  return <div>
+    <NetFlixSeries/>
+    <NetFlixSeries/>
+    <NetFlixSeries/>
+    <NetFlixSeries/>
+    <NetFlixSeries/>
+  </div>
+};
 
 export default App;
+
+export const NetFlixSeries = () => {
+  return (
+    <div style={{textAlign:"center",fontSize:"20px"}}>
+      <div style={{textAlign:"center"}}>
+        <img src="1.jpg" alt="error" height={400} width={600} />
+      </div>
+      <p><strong>Name:</strong>Queen of tears</p>
+      <p><strong>Rating:8.9</strong></p>
+      <p>
+       <strong>Summary:</strong> Queen of Tears (Korean: 눈물의 여왕) is a 2024 South Korean
+        television series written by Park Ji-eun, co-directed by Jang Young-woo
+        [ko] and Kim Hee-won, and starring Kim Soo-hyun, Kim Ji-won, Park
+        Sung-hoon, Kwak Dong-yeon, and Lee Joo-bin. The series depicts the story
+        of a married couple in a crisis. It aired on tvN from March 9, to April
+        28, 2024, every Saturday and Sunday at 21:20 (KST). It is also available
+        for streaming on TVING in South Korea, and on Netflix in selected
+        regions. Queen of Tears reached a nationwide rating of 24.850% for its
+        final episode and became the highest-rated tvN series, surpassing Crash
+        Landing on You.[4] It also became the third highest-rated series in
+        Korean cable television history for viewership ratings and the second
+        highest by number of viewers.
+      </p>
+      <hr/>
+    </div>
+  );
+};
