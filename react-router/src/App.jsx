b16c03dd6e2fcd063f437020644430c759c1c3d1
Laxman-Nath <nathlaxman242@gmail.com>
2024-12-10 15:43:45 +0545

    setting up routes using createroutesfromelements

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,17 +1,27 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    { path: "/about", element: <About /> },
-    { path: "/movie", element: <Movie /> },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   { path: "/about", element: <About /> },
+  //   { path: "/movie", element: <Movie /> },
+  // ]);
+
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/movie" element={<Movie/>}/>
+      </Route>
+    )
+  )
 
   return <RouterProvider router={router}/>
 }
